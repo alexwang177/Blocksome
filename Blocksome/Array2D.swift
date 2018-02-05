4dd5949c216f6263296167e0b745a23ef4c172ba
Alex Wang <alexwang177@gmail.com>
2018-02-05 13:17:25 -0600

    Added Array2D class

---
+++ Blocksome/Array2D.swift
@@ -0,0 +1,28 @@
+//
+//  Array2D.swift
+//  Blocksome
+//
+//  Created by AlexWang1 on 2/5/18.
+//  Copyright © 2018 Alex Wang. All rights reserved.
+//
+
+struct Array2D<T> {
+    let columns: Int
+    let rows: Int
+    fileprivate var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(repeating: nil, count: rows*columns)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
