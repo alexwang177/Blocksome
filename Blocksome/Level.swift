09975b0781ae72877d1869faab3ad32e539602e4
Alex Wang <alexwang177@gmail.com>
2018-02-07 13:03:38 -0600

    Added extension (Dictionary) file and also added init (filename: ) to Level.swift

--- Blocksome/Level.swift
+++ Blocksome/Level.swift
@@ -52,4 +52,36 @@ class Level {
         }
         return set
     }
+    
+    init(filename: String) {
+        // 1
+        guard let dictionary = Dictionary<String, AnyObject>.loadJSONFromBundle(filename: filename) else { return }
+        // 2
+        guard let tilesArray = dictionary["tiles"] as? [[Int]] else { return }
+        // 3
+        for (row, rowArray) in tilesArray.enumerated() {
+            // 4
+            let tileRow = NumRows - row - 1
+            // 5
+            for (column, value) in rowArray.enumerated() {
+                if value == 1 {
+                    tiles[column, tileRow] = Tile()
+                }
+            }
+        }
+    }
+    
+/* Load the named file into a Dictionary using the loadJSONFromBundle(filename:) helper function that you just added. Note that this function may return nil -- it returns an optional -- and here you use a guard to handle this situation.
+ 
+     The dictionary has an array named "tiles". This array contains one element for each row of the level. Each of those row elements is itself an array containing the columns for that row. The type of tilesArray is therefore array-of-array-of-Int, or [[Int]].
+ 
+     Step through the rows using built-in enumerated() function, which is useful because it also returns the current row number.
+ 
+     In Sprite Kit (0, 0) is at the bottom of the screen, so you have to reverse the order of the rows here. The first row you read from the JSON corresponds to the last row of the 2D grid.
+ 
+     Step through the columns in the current row. Every time it finds a 1, it creates a Tile object and places it into the tiles array.*/
+    
+    
+    
+
 }
